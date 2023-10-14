d6a67fccbf5dfbd76c4d92983ff0a9021ca79abe
MissYetunde <surajudeenyetunde@gmail.com>
2023-10-14 17:01:24 +0000

    Tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,16 +12,19 @@ from models.base_model import BaseModel
 
 class TestBaseModel_instance(unittest.TestCase):
 
+	def setUp(self):
+		""" setup for basemodel """
+		self.base_model_instance = BaseModel()
    
-    def test_unique_id(self):
-        """ Test if each id has a unique ID """
-        print("test1")
-        instance_1 = BaseModel()
-        self.assertEqual(self.base_model_instance.id, instance_1.id)
+	def test_unique_id(self):
+		""" Test if each id has a unique ID """
+		print("test1")
+		instance_1 = BaseModel()
+		self.assertEqual(self.base_model_instance.id, instance_1.id)
 	
-    def test_id_type(self):
-        """ test the attribute of id if its type str """ 
-        self.assertIsInstance(self.base_model_instance.id, str)
+	def test_id_type(self):
+		""" test the attribute of id if its type str """ 
+		self.assertIsInstance(self.base_model_instance.id, str)
 
 
 if __name__ == '__main__':
